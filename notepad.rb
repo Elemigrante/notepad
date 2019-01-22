47dc1f84e51af4e889c817671310ad0cfaee210f
Neladnov Dmitry <gasdom@yandex.ru>
2019-01-22 14:24:39 +0300

    initial commit

---
+++ notepad.rb
@@ -0,0 +1,24 @@
+require_relative 'link'
+require_relative 'memo'
+require_relative 'task'
+
+puts 'Привет, я твой блокнот!'
+puts 'Что хотите записать в блокнот?'
+
+choices = Post.post_types
+
+choice = -1
+
+until choice >= 0 && choice < choices.size
+  choices.each_with_index do |type, index|
+    puts "\t#{index}. #{type}"
+  end
+
+  choice = STDIN.gets.chomp.to_i
+end
+
+entry = Post.create(choice)
+entry.read_from_console
+entry.save
+
+puts 'Ура, запись сохранена!'
