c4b7580ca9d8f3e4795d7768cf93348f1324ba0e
Neladnov Dmitry <gasdom@yandex.ru>
2019-02-11 16:20:33 +0300

    v.1.5 + SQLite

--- post.rb
+++ post.rb
@@ -1,11 +1,15 @@
+require 'sqlite3'
+
 class Post
 
+  @@SQLITE_DB_FILE = 'notepad.sqlite'
+
   def self.post_types
-    [Memo, Link, Task]
+    { 'Memo' => Memo, 'Link' => Link, 'Task' => Task }
   end
 
-  def self.create(type_index)
-    post_types[type_index].new
+  def self.create(type)
+    post_types[type].new
   end
 
   def initialize
@@ -22,15 +26,43 @@ class Post
   def save
     file = File.new(file_path, 'w:UTF-8')
 
-    to_strings.each {|string| file.puts(string)}
+    to_strings.each { |string| file.puts(string) }
     file.close
   end
 
   def file_path
-    current_path = File.dirname(__FILE__ )
+    current_path = File.dirname(__FILE__)
 
     file_name = @created_at.strftime("%Y-%m-%d_%H-%M-%S.txt")
 
     "#{current_path}/#{self.class.name}_#{file_name}.txt"
+  end
+
+  def save_to_db
+    db = SQLite3::Database.open(@@SQLITE_DB_FILE)
+    db.results_as_hash = true
+
+    db.execute(
+      "INSERT INTO posts (" +
+        to_db_hash.keys.join(',') +
+        ")" +
+        " VALUES (" +
+        ('?,' * to_db_hash.keys.size).chomp(',') +
+        ")",
+      to_db_hash.values
+    )
+
+    insert_row_id = db.last_insert_row_id
+
+    db.close
+
+    insert_row_id # return
+  end
+
+  def to_db_hash
+    {
+      'type' => self.class.name,
+      'create_at' => @created_at.to_s
+    }
   end
 end
